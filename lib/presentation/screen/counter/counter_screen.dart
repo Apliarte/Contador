33c81f25d80add6c5b29e802e34259abcb8b04c6
ApliArte <erbolamm@gmail.com>
2023-06-18 07:31:43 +0200

    contador diseño de pantalla

--- lib/presentation/screen/counter/counter_screen.dart
+++ lib/presentation/screen/counter/counter_screen.dart
@@ -6,7 +6,18 @@ class CounterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Counter Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+          Text('10',style: TextStyle(fontSize: 160,fontWeight: FontWeight.w100),),
+          Text('Clicks',style: TextStyle(fontSize: 25))
+        ],),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.plus_one),
+      ),
     );
   }
 }
